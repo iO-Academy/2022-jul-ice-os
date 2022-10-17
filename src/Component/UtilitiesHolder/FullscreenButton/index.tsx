2606aa8c4f52ab0e6a6d9163cb66728d8f5ddd68
Mike Oram <Mikeoram01@gmail.com>
2022-10-17 15:03:24 +0100

    think this has fixed the fullscreen issue

---
+++ src/Component/UtilitiesHolder/FullscreenButton/index.tsx
@@ -0,0 +1,21 @@
+import './FullScreenButton.css'
+import screenfull from "screenfull";
+
+
+
+const FullScreenButton = ():JSX.Element => {
+
+    const root = document.getElementById('root');
+
+   const makeFullscreen = () => {
+        screenfull.toggle(root!);
+    }
+
+    return (
+        <div className="FullScreenButton">
+            <i onClick={makeFullscreen} className="fa-solid fa-maximize" id="fullscreenButton"></i>
+        </div>
+    )
+}
+
+export default FullScreenButton
