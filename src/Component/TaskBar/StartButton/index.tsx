3e4641ed9c783666fcb0179bd25a2429b5ffc803
Mike Oram <Mikeoram01@gmail.com>
2022-10-17 11:50:49 +0100

    Added the start button

---
+++ src/Component/TaskBar/StartButton/index.tsx
@@ -0,0 +1,12 @@
+import './StartButton.css';
+
+const StartButton = (): JSX.Element => {
+
+    return (
+        <button>
+            Start
+        </button>
+    )
+}
+
+export default StartButton
