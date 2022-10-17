6aa9efe3f8c67222fd4725afcd498582ca79d81c
Josh Bennet <josh@thebennets.net>
2022-10-17 11:33:14 +0100

    added the basic struct for bothe index and scss

--- src/Desktop/index.tsx
+++ src/Desktop/index.tsx
@@ -1,3 +1,14 @@
-import './Desktop.scss'
+import './Desktop.css'
 // TODO Big D for desktop, also big I for index??
 
+const Desktop = (): JSX.Element => {
+
+
+    return (
+        <main  className={"desktop"}>
+
+        </main>
+    )
+}
+
+export default Desktop
