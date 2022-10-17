1e17c27eb877a04ed27e5e033cd240e9807e14dc
Josh Bennet <josh@thebennets.net>
2022-10-17 12:16:06 +0100

    trying in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Desktop from "./Desktop";
 import TaskBar from "./Component/TaskBar";
 
 function App() {
   return (
     <div className="App">
+        <Desktop/>
       <TaskBar/>
     </div>
   );
