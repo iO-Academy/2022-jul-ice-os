244a34cde4c8ffb33aba02c1a049efc6cd3a5782
Mike Oram <Mikeoram01@gmail.com>
2022-10-17 14:25:21 +0100

    Added the logic for screenfull to enable fullscreen as a toggle

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,8 @@ import React from 'react';
 import './App.css';
 import Desktop from "./Component/Desktop";
 import TaskBar from "./Component/TaskBar";
-import screenfull from "screenfull";
 
 function App() {
-    const app = document.getElementById('App');
-    const fullscreenButton = document.getElementById('fullscreenButton');
-
-    fullscreenButton!.addEventListener('click', () => {
-        if(screenfull.isEnabled) {
-            screenfull.toggle(app!);
-        }
-    })
 
   return (
     <div className="App" id="App">
--- src/Component/UtilitiesHolder/FullScreenButton/index.tsx
+++ src/Component/UtilitiesHolder/FullScreenButton/index.tsx
@@ -1,7 +1,20 @@
 
 import './FullScreenButton.css'
+import screenfull from "screenfull";
 
 const FullScreenButton = ():JSX.Element => {
+
+    const app = document.getElementById('App');
+    const fullscreenButton = document.getElementById('fullscreenButton');
+
+    fullscreenButton!.addEventListener('click', () => {
+        if(screenfull.isEnabled) {
+            screenfull.toggle(app!);
+        }
+    })
+
+
+
     return (
         <div className="FullScreenButton">
             <i className="fa-solid fa-maximize" id="fullscreenButton"></i>
