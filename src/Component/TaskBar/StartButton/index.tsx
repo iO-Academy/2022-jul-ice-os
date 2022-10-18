147a056c4dfabc29d72570499ab17ecc9c8270e6
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-10-18 11:43:57 +0100

    menu display works on click

--- src/Component/TaskBar/StartButton/index.tsx
+++ src/Component/TaskBar/StartButton/index.tsx
@@ -4,28 +4,29 @@ import React, {useState} from "react";
 
 const StartButton = (): JSX.Element => {
     const [toggle, setToggle] = React.useState(false)
-    const [hidden, setHidden] = React.useState(null) // :(
+    const [width, setWidth] = React.useState(null)
 
     const showMenu = () => {
         setToggle(!toggle);
         if (toggle === true) {
             // @ts-ignore
-            document.getElementById( "menu").style.display="none";
+           document.getElementById( "menu").style.display="none";
         } else {
            // @ts-ignore
-            document.getElementById("menu").style.display="block";
+            setWidth("block");document.getElementById("menu").style.display="block";
         }
     }
 
-    // @ts-ignore
-    // @ts-ignore
+
     return (
-        <div className={"startMenu"}>
+        <div className="startMenu">
+            <div>
         <button onClick={showMenu} className="startButton">
             <img className="startButtonImage" src={"../../../assets/Desktop/Images/icebergStartMenuIcon.png"} alt="Iceberg start menu logo" />
             <p className="startButtonText">Start</p>
         </button>
-            <div id="menu" > // :(
+            </div>
+            <div className="menuDisplay" id="menu">
                 <li>text</li>
                 <li>text</li>
                 <li>text</li>
