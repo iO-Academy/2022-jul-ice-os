1d8e2718e49db80fab647804d297eb34493a5dd7
Josh Bennet <josh@thebennets.net>
2022-10-18 14:04:00 +0100

    done more styling on the start menu

--- src/Component/TaskBar/StartButton/index.tsx
+++ src/Component/TaskBar/StartButton/index.tsx
@@ -1,7 +1,6 @@
 import './StartButton.css';
 import React, {useState} from "react";
 
-
 const StartButton = (): JSX.Element => {
     const [toggle, setToggle] = React.useState(false)
     const [width, setWidth] = React.useState(null)
@@ -17,22 +16,24 @@ const StartButton = (): JSX.Element => {
         }
     }
 
-
     return (
         <div className="startMenu">
             <div>
         <button onClick={showMenu} className="startButton">
             <img className="startButtonImage" src={"../../../assets/Desktop/Images/icebergStartMenuIcon.png"} alt="Iceberg start menu logo" />
-            <p className="startButtonText">Start</p>
+            <p className="startButtonText">start</p>
         </button>
             </div>
             <div className="menuDisplay" id="menu">
-                <div className={"menuSpacer"}></div>
-                <ul>text</ul>
-                <ul>text</ul>
-                <ul>text</ul>
-                <ul>text</ul>
-                <div className={"menuSpacer2"}></div>
+                <div className={"menuSpacer"}>
+                    <img className={"menuImage"} src={"../../../assets/Desktop/Images/menuImage.png"} alt="Iceberg start menu logo" />
+                    <p className={"userInMenu"}>Nico</p>
+                </div>
+                <ul>It</ul>
+                <ul>is</ul>
+                <ul>so</ul>
+                <ul>beautiful</ul>
+                <div className={"menuSpacer2"}>There is no log out, you are here forever</div>
             </div>
         </div>
     )
