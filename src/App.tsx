32435581075289d6fc524b3ff49f51ae56670809
Johanna <Johannaloe@gmail.com>
2022-10-18 11:24:32 +0100

    installed npm draggable, added import in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Desktop from "./Component/Desktop";
 import TaskBar from "./Component/TaskBar";
+import Draggable from 'react-draggable'
 
 function App() {
 
