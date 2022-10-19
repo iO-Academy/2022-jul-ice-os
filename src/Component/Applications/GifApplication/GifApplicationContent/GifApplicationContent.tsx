abb741a35f15129aa0e654b2e1e63efaf261d634
Mike Oram <Mikeoram01@gmail.com>
2022-10-19 14:05:46 +0100

    gif api fetch working - styling and functionality needed

--- src/Component/Applications/GifApplication/GifApplicationContent/GifApplicationContent.tsx
+++ src/Component/Applications/GifApplication/GifApplicationContent/GifApplicationContent.tsx
@@ -3,13 +3,13 @@ import {useEffect, useState} from "react";
 
 const GifApplicationContent = () => {
 
-    const [storedGifData, setStoredGifData] = useState()
+    const [storedGifData, setStoredGifData] = useState("")
 
     const getGifData = async () => {
         let fetchUrl = 'https://api.giphy.com/v1/gifs/random?api_key=Glou4K43Jc8huvJDGqjH4mMQY4iG8bZQ&rating=g'
         const gifData = await fetch(fetchUrl);
         const jsonGifData = await gifData.json()
-        setStoredGifData(jsonGifData.data.url)
+        setStoredGifData(jsonGifData.data.images.original.url)
     }
 
     useEffect(() => {
@@ -19,7 +19,7 @@ const GifApplicationContent = () => {
 
     return (
         <div>
-            <p>{storedGifData}</p>
+            <img src={storedGifData} alt={"a random giphy"}/>
         </div>
 
     )
