a1cf6481a8df4af348bf7adb93134386be08e488
Mike Oram <Mikeoram01@gmail.com>
2022-10-19 12:27:14 +0100

    working on the fetch and minor CSS changes

--- src/Component/Applications/GifApplication/GifApplicationContent/GifApplicationContent.tsx
+++ src/Component/Applications/GifApplication/GifApplicationContent/GifApplicationContent.tsx
@@ -1,6 +1,11 @@
 import './GifApplicationContent.css'
+import {useEffect} from "react";
 
 const GifApplicationContent = () => {
+    useEffect(() => {
+        fetch('api.giphy.com/v1/gifs/random?api_key=Glou4K43Jc8huvJDGqjH4mMQY4iG8bZQ&rating=g')
+    }, [])
+
     return (
         <div></div>
     )
