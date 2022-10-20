1937477cacac5e93eff440279e3508eb1db1d6bc
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-10-20 12:20:28 +0100

    reformat code for readibility

--- src/Component/Desktop/AppShortcuts/AppShortcuts.tsx
+++ src/Component/Desktop/AppShortcuts/AppShortcuts.tsx
@@ -10,7 +10,8 @@ interface AppShortcutsProps {
     setGifIsOpen: Function
     setGifIsRunning: Function
 }
-const AppShortcuts = (props:AppShortcutsProps) => {
+
+const AppShortcuts = (props: AppShortcutsProps) => {
 
     const handleClickAbout = () => {
         props.setAboutIsOpen(true)
@@ -34,16 +35,21 @@ const AppShortcuts = (props:AppShortcutsProps) => {
 
     return (
         <div className={"appShortcuts"}>
-            <div className={"app"}> <img onClick={handleClickAbout} src={"../../../assets/Desktop/Images/AboutImage.png"}alt={"About Icon"}/>
-            <p>About</p>
+            <div className={"app"}><img onClick={handleClickAbout} src={"../../../assets/Desktop/Images/AboutImage.png"}
+                                        alt={"About Icon"}/>
+                <p>About</p>
             </div>
-            <div className={"app"}> <img onClick={handleClickSurf} id={"surfwaveIcon"} src={"../../../assets/Desktop/Images/surfingPenguinIcon.png"}alt={"Surfwave Fury Icon"}/>
+            <div className={"app"}><img onClick={handleClickSurf} id={"surfwaveIcon"}
+                                        src={"../../../assets/Desktop/Images/surfingPenguinIcon.png"}
+                                        alt={"Surfwave Fury Icon"}/>
                 <p>Surfwave Fury</p>
             </div>
-            <div className={"app"}> <img onClick={handleClickMusic} src={"../../../assets/Desktop/Images/musicPenguin.png"}alt={"Music Icon"}/>
+            <div className={"app"}><img onClick={handleClickMusic}
+                                        src={"../../../assets/Desktop/Images/musicPenguin.png"} alt={"Music Icon"}/>
                 <p>Music</p>
             </div>
-            <div className={"app"}> <img onClick={handleClickGif} id={"gifsicon"}src={"../../../assets/Desktop/Images/gifWatchingPenguin.png"}alt={"Gif Icon"}/>
+            <div className={"app"}><img onClick={handleClickGif} id={"gifsicon"}
+                                        src={"../../../assets/Desktop/Images/gifWatchingPenguin.png"} alt={"Gif Icon"}/>
                 <p>Gifs</p>
             </div>
         </div>
